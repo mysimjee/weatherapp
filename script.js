a7df0cd38a1661280a20dd454b3d899e2ccde1ee
mysimjee <119560224+mysimjee@users.noreply.github.com>
2022-12-09 22:24:31 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let weatherinfo = [document.getElementById("city-name"), document.getElementById
 async function getweatherinformation(city) {
     const URL = "https://api.openweathermap.org/data/2.5/weather";
     const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
-    let getinfo = await fetch(FULL_URL).then(response => response.json())
+    let getinfo = await fetch(FULL_URL).then(response => response.json()).catch(err => console.error(err))
     let weathertype = getinfo.weather[0].main
     let cityname = getinfo.name
     const weatherdata = getinfo.main
@@ -16,7 +16,7 @@ async function getweatherinformation(city) {
 
 }
 
-function searchdata() {
+async function searchdata() {
     let inputcity = document.getElementById("city-input")
     let city = inputcity.value
     try {
@@ -25,8 +25,13 @@ function searchdata() {
         weatherinfo[2].innerText = "Searching"
         weatherinfo[3].innerText = "Searching"
         weatherinfo[4].innerText = "Searching"
-        return getweatherinformation(city)
+        return await getweatherinformation(city)
     } catch(error) {
-        weatherinfo[0].innerText = "Failed To Retrieve Weather Information"
+        weatherinfo[0].innerText = "Wrong Input Possible"
+        weatherinfo[1].innerText = "Failed To Retrieve Weather Information"
+        weatherinfo[2].innerText = "No Data Found"
+        weatherinfo[3].innerText = "No Data Found"
+        weatherinfo[4].innerText = "No Data Found"
+        console.error(error)
     }
 }
